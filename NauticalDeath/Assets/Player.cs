f160aeee863b34cd1f82329b9ac4fc14e366d0bb
CyberBlast <ashishdantesparda@gmail.com>
2019-07-07 17:24:38 +0530

    08: Implemented movement.

---
+++ NauticalDeath/Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	[Tooltip("In m/s")][SerializeField] float xSpeed = 4f;
+	[Tooltip("In m")]  [SerializeField] float xRange = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xThrow  = CrossPlatformInputManager.GetAxis("Horizontal");
+		float xOffset = xSpeed * xThrow * Time.deltaTime;
+		float xRawPos = transform.localPosition.x + xOffset;
+		float xPos = Mathf.Clamp(xRawPos, -xRange, +xRange);
+		transform.localPosition = new Vector3(xPos, transform.localPosition.y, transform.localPosition.z);
+	}
+}
