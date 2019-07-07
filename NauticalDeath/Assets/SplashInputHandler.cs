f751e972ad0945679ee98da8e93891d83e76b0d7
CyberBlast <ashishdantesparda@gmail.com>
2019-07-07 12:04:06 +0530

    06: Added Splash Screen and backgroung music

---
+++ NauticalDeath/Assets/SplashInputHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SplashInputHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		Invoke("LoadLevel", 2f);
+    }
+
+	void LoadLevel()
+	{
+		DontDestroyOnLoad(this);
+		SceneManager.LoadScene(1);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
