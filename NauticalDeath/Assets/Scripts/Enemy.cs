de2e52ff3608ed46bea0fd5840bfb71aba5a935a
CyberBlast <ashishdantesparda@gmail.com>
2019-07-09 17:25:27 +0530

    14: Enemies die when colliding with particles.

---
+++ NauticalDeath/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	void Start()
+	{
+		AddBoxCollision();
+	}
+
+	private void AddBoxCollision()
+	{
+		if (GetComponent<BoxCollider>()) return;
+		BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
+		boxCollider.isTrigger = false;
+	}
+
+	void OnParticleCollision(GameObject other)
+	{
+		Destroy(gameObject);
+	}
+}
